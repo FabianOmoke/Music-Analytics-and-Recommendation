7338b9f410599c0629bb934c7cf29979850e31e9
Fabian_Omoke <f.ombui@blue-planet-ecosystems.com>
2022-10-06 12:12:57 +0200

    added search button

--- music_analytics.py
+++ music_analytics.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
+st.header('Music Analytics App') 
 search_options = ['Track', 'Artist', 'Album' ]
-st.sidebar.selectbox('Your Search Choice Please', search_options)
-st.header('Music Analytics App')
+search_selection = st.sidebar.selectbox('Your Search Choice Please', search_options)
+
+st.text_input(search_selection + "[Keyword Search]")
+Search_button = st.button("Search")
+
