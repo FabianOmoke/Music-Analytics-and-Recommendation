8d10fb4ededad5f4c39a9024e5fd4c74c2c13c88
Fabian_Omoke <f.ombui@blue-planet-ecosystems.com>
2022-10-11 19:07:07 +0200

    added search print

--- music_analytics.py
+++ music_analytics.py
@@ -20,17 +20,31 @@ Search_button = st.button("Search")
 if search_input is not None and len (search_input)> 0:
     if search_selection == "Track":
         st.write("Search for Track")
+        tracks = sp.search(q='track:'+search_input,type='artist', limit=10)
+        tracks_list = tracks['tracks']['items']
+        if len(tracks_list)> 0:
+            for track in tracks_list:
+                st.write(track['name'] + "- By - " + track['artists'][0]['name'])
+
+
     elif search_selection == 'Artist':
+
         st.write("Search for Artist")
-        artist = sp.search(q='artist:'+search_input,type='artist', limit=10)
+        artist = sp.search(q='artist:'+ search_input,type='artist', limit=10)
+        artist_list = artist['artist']['items']
+        if len(artist_list)> 0:
+            for artist in artist_list:
+                st.write(artist['name'] + "- By - " + artist['artists'][0]['name'])
+               
+
     elif search_selection == 'Album':
         st.write("Search for Album")
         albums = sp.search(q='album:'+search_input,type='album', limit=20)
         album_list = albums['albums']['items']
         if len(album_list)> 0:
             for album in album_list:
-                print(album['name'] + "- By - " + album['artists'][0]['name'])
-                print('Track ID:' + album['id'] + " / Artist ID -" + album['artists'][0]['id'])
-                print("----------")
+                st.write(album['name'] + "- By - " + album['artists'][0]['name'])
+                #print('Track ID:' + album['id'] + " / Artist ID -" + album['artists'][0]['id'])
+         
 
 
